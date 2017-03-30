6ad00888d626d2510ce9fd80e3ddd14804f1a3d3
Swall0w <technext.jpn@gmail.com>
2017-03-30 23:40:54 +0900

    subprocess

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 git clone https://github.com/pjreddie/darknet.git
+(
 cd darknet
 make
 ./darknet
+)
+echo 'Darkent is OK'
+echo 'Training YOLOv2 with VOC Data'
+mkdir data
+(
+cd data
+pwd
+)
